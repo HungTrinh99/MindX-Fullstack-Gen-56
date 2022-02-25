e2b406fb386628bb1723b4f7a236d9b001e384f5
hungtrinhhyper <hypertal.tech@gmail.com>
2022-02-25 19:57:35 +0700

    Complete basic flow of Cart Project

--- lesson-3/src/SmartPhoneStore/ProductDetail.js
+++ lesson-3/src/SmartPhoneStore/ProductDetail.js
@@ -1,9 +1,60 @@
 import React from "react";
 
 const ProductDetail = (props) => {
+  const { selectedPhone } = props;
 
+  if (!selectedPhone) {
+    return <h4>Chưa có dữ liệu</h4>;
+  }
+  const {
+    name,
+    img,
+    ram,
+    rom,
+    info: { screen, os, frontCamera, backCamera },
+  } = selectedPhone;
 
-  return <div></div>;
+  return (
+    <div className="row mt-4">
+      <div className="col-4 card">
+        <h5>{name}</h5>
+        <img src={img} alt={name} className="card-img-top" />
+      </div>
+      <div className="col-8">
+        <div className="table">
+          <thead>
+            <tr colSpan="2">
+              <h3>Thông số kỹ thuật</h3>
+            </tr>
+          </thead>
+          <tr>
+            <td>Màn hình</td>
+            <td>{screen}</td>
+          </tr>
+          <tr>
+            <td>Hệ điều hành</td>
+            <td>{os}</td>
+          </tr>
+          <tr>
+            <td>Camera trước</td>
+            <td>{frontCamera}</td>
+          </tr>
+          <tr>
+            <td>Camera sau</td>
+            <td>{backCamera}</td>
+          </tr>
+          <tr>
+            <td>Ram</td>
+            <td>{ram}</td>
+          </tr>
+          <tr>
+            <td>Rom</td>
+            <td>{rom}</td>
+          </tr>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
--- lesson-3/src/SmartPhoneStore/SmartPhoneStore.js
+++ lesson-3/src/SmartPhoneStore/SmartPhoneStore.js
@@ -1,60 +1,64 @@
 import React, { Component } from "react";
 import ProductDetail from "./ProductDetail";
 import ProductList from "./ProductList";
-
+const productData = [
+  {
+    id: 1,
+    name: "Samsung Galaxy A10",
+    price: "40906000",
+    img: "images/samsung-note.jpg",
+    info: {
+      screen: "AMOLED Full HD 9.0",
+      os: "Android 9.0",
+      frontCamera: "20MP",
+      backCamera: "Chính 48MB, phụ 30MP",
+    },
+    ram: "4 GB",
+    rom: "64 GB",
+  },
+  {
+    id: 2,
+    name: "IPhone12",
+    price: "200306000",
+    img: "images/iphone.jpg",
+    info: {
+      screen: "Full HD 12.0",
+      os: "IOS 14",
+      frontCamera: "20MP",
+      backCamera: "Chính 100MB, phụ 30MP",
+    },
+    ram: "16 GB",
+    rom: "32 GB",
+  },
+  {
+    id: 3,
+    name: "Xiaomi Note 10",
+    price: "10005000",
+    img: "images/xioami.jpg",
+    info: {
+      screen: "OLED 10.0",
+      os: "Android 8.0",
+      frontCamera: "69MP",
+      backCamera: "Chính 96MB, phụ 30MP",
+    },
+    ram: "10 GB",
+    rom: "64 GB",
+  },
+];
 class SmartPhoneStore extends Component {
   state = {
-    products: [
-      {
-        id: 1,
-        name: "Samsung Galaxy A10",
-        price: "40906000",
-        img: "images/samsung-note.jpg",
-        info: {
-          screen: "AMOLED Full HD 9.0",
-          os: "Android 9.0",
-          frontCamera: "20MP",
-          backCamera: "Chính 48MB, phụ 30MP",
-        },
-        ram: "4 GB",
-        rom: "64 GB",
-      },
-      {
-        id: 2,
-        name: "IPhone12",
-        price: "200306000",
-        img: "images/iphone.jpg",
-        info: {
-          screen: "Full HD 12.0",
-          os: "IOS 14",
-          frontCamera: "20MP",
-          backCamera: "Chính 100MB, phụ 30MP",
-        },
-        ram: "16 GB",
-        rom: "32 GB",
-      },
-      {
-        id: 3,
-        name: "Xiaomi Note 10",
-        price: "10005000",
-        img: "images/xioami.jpg",
-        info: {
-          screen: "OLED 10.0",
-          os: "Android 8.0",
-          frontCamera: "69MP",
-          backCamera: "Chính 96MB, phụ 30MP",
-        },
-        ram: "10 GB",
-        rom: "64 GB",
-      },
-    ],
-    selectedPhone: {},
+    products: productData,
+    selectedPhone: null,
   };
 
   onPhoneSelect = (id) => {
     // 1. Tìm kiếm id này là sản phẩm nào trong list
     // 2. SetState cho selected phone
-    console.log("Parent Component - ID-", id);
+    const { products } = this.state;
+    const selectedPhone = products.find((phone) => phone.id === id);
+    this.setState({
+      selectedPhone: selectedPhone,
+    });
   };
 
   render() {
@@ -77,6 +81,9 @@ class SmartPhoneStore extends Component {
 export default SmartPhoneStore;
 
 /*
+  ------------------------
+  -----COMPONENT TREE-----
+  ------------------------
   SmartPhoneStore
       ProductList
           ProductItem
@@ -86,3 +93,6 @@ export default SmartPhoneStore;
           ....
       ProductDetail
 */
+
+// Set State: cơ chế merging
+// useState: replacing
